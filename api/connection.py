53989712db304253506d4e343a3d8297bdb741ee
PeiYing <peggy2000000@gmail.com>
2022-12-16 15:23:03 +0800

    feat database connection file

--- api/connection.py
+++ api/connection.py
@@ -4,9 +4,10 @@ import os
 
 load_dotenv() # take enviroment variables from .env
 database_password=os.getenv("database_password")
+database_user=os.getenv("database_user")
 
 taipei_attractions = {
-    "user":"root",
+    "user":database_user,
     "password":database_password,
     "host":"127.0.0.1",
     "database":"taipei_attractions",
