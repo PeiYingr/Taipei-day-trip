d3895506910911d31a4b0bcb6099110e4223ffe3
PeiYing <peggy2000000@gmail.com>
2022-12-16 16:49:11 +0800

    modified js of booking

--- static/JS/booking.js
+++ static/JS/booking.js
@@ -24,34 +24,35 @@ fetch("/api/user/auth").then(function(response){    //method:"GET"
         const username = data.data.name;
         const useremail = data.data.email;
         usernameText.innerHTML = username;
-        fetch("/api/booking").then(function(response){  //method:"GET"
-            return response.json();  
-        }).then(function(data){
-            if(data.data == null){
-                return;
-            }
-            else{
-                showReservation.style.display="block";
-                noReservation.style.display="none";
-                afterfooter.style.display="none";
-                const img = document.createElement("img");        
-                img.setAttribute("src",data.data.attraction.image);
-                picture.appendChild(img);
-                attractionName.innerHTML = data.data.attraction.name;
-                date.innerHTML = data.data.date;
-                if (data.data.time=="上半天"){
-                    time.innerHTML = "早上9點到下午4點";
-                }else{
-                    time.innerHTML = "下午1點到晚上8點";
-                }
-                price.innerHTML = "新台幣"+data.data.price+"元";
-                address.innerHTML = data.data.attraction.address;
-                nameInput.setAttribute("value", username);
-                emailInput.setAttribute("value", useremail);
-                totalPrice.innerHTML = "新台幣"+data.data.price+"元";                  
-            }           
-        })
     }
+})
+
+fetch("/api/booking").then(function(response){  //method:"GET"
+    return response.json();  
+}).then(function(data){
+    if(data.data == null){
+        return;
+    }
+    else{
+        showReservation.style.display="block";
+        noReservation.style.display="none";
+        afterfooter.style.display="none";
+        const img = document.createElement("img");        
+        img.setAttribute("src",data.data.attraction.image);
+        picture.appendChild(img);
+        attractionName.innerHTML = data.data.attraction.name;
+        date.innerHTML = data.data.date;
+        if (data.data.time=="上半天"){
+            time.innerHTML = "早上9點到下午4點";
+        }else{
+            time.innerHTML = "下午1點到晚上8點";
+        }
+        price.innerHTML = "新台幣"+data.data.price+"元";
+        address.innerHTML = data.data.attraction.address;
+        nameInput.setAttribute("value", username);
+        emailInput.setAttribute("value", useremail);
+        totalPrice.innerHTML = "新台幣"+data.data.price+"元";                  
+    }           
 })
 
 // Part 5 - 5：delete booking API
