c024938bfbb21aae857d8230fc008d2b8100e04f
PeiYing <peggy2000000@gmail.com>
2022-12-23 20:57:31 +0800

    Create environment variables

--- data/taipei-attractions.py
+++ data/taipei-attractions.py
@@ -1,8 +1,15 @@
 import json
 import mysql.connector
+import mysql.connector
+from  dotenv import load_dotenv
+import os
 
-taipei_attractions=mysql.connector.connect(user="root", 
-                                password="hihi3838",
+load_dotenv()
+database_password=os.getenv("database_password")
+database_user=os.getenv("database_user")
+
+taipei_attractions=mysql.connector.connect(user=database_user, 
+                                password=database_password,
                                 host="127.0.0.1",
                                 database="taipei_attractions")
 cursor=taipei_attractions.cursor()
